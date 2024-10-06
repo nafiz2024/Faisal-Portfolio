b08a7cd6f675f034140ee1c699373a9a2c7076ea
nafiz2024 <nafizalam.dev@gmail.com>
2024-10-07 02:08:00 +0600

    Add files via upload

---
+++ JS/tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+  theme: {
+    gridTemplateColmuns: {
+      auto: "repeat(auto-fit, minmax(200px, 1fr))",
+    },
+    animation: {
+      spin_slow: "spin 6s linear infinite",
+    },
+  },
+};
